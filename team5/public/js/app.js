7c3d48d7e5136a15634e26a8529f7f504e11c912
Majito Brinkitos <mcvef2013@gmail.com>
2025-02-24 11:12:04 -0800

    layout design 202-02-24

--- team5/public/js/app.js
+++ team5/public/js/app.js
@@ -1,9 +1,56 @@
-//
-    document.addEventListener('DOMContentLoaded', () =>{
-        document.querySelectorAll('.icon-plus').forEach(button => {
-        button.addEventListener('click', () => {
-            const cardBody = button.closest('.card').querySelector('.card-body');
-            cardBody.classList.toggle('hidden');
-        });
-    });
-});
+
+    // Function to handle dragging of tasks
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+// Function to allow dropping of tasks
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+// Function to handle dropping of tasks
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.currentTarget.appendChild(document.getElementById(data));
+}
+
+// Function to toggle visibility of task creation and progress sections
+function createTask() {
+  var inProgress = document.getElementById("inprogress");
+  var done = document.getElementById("done");
+  var newTask = document.getElementById("new-task");
+  if (inProgress.style.display === "none") {
+    inProgress.style.display = "block";
+    done.style.display = "block";
+    newTask.style.display = "none";
+  } else {
+    inProgress.style.display = "none";
+    done.style.display = "none";
+    newTask.style.display = "flex";
+  }
+}
+
+// Function to save new tasks
+function saveTask() {
+  var todo = document.getElementById("wish");
+  var taskName = document.getElementById("form-row-name").value;
+  todo.innerHTML += `
+    <div class="task" id="${taskName.toLowerCase().split(" ").join("")}" draggable="true" ondragstart="drag(event)">
+      <span>${taskName}</span>
+    </div>`;
+}
+
+// Function to toggle visibility of save and edit buttons
+function editTask() {
+  var saveButton = document.getElementById("save-btn");
+  var editButton = document.getElementById("edit-btn");
+  if (saveButton.style.display === "none") {
+    saveButton.style.display = "block";
+    editButton.style.display = "none";
+  } else {
+    saveButton.style.display = "none";
+    editButton.style.display = "block";
+  }
+}
